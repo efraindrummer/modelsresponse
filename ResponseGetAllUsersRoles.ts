7fd8253dfb503875718c08326d22f9c91532032e
Efrain May <58404444+efraindrummer@users.noreply.github.com>
2023-01-24 14:29:33 -0600

    Create ResponseGetAllUsersRoles.ts

---
+++ ResponseGetAllUsersRoles.ts
@@ -0,0 +1,11 @@
+export interface ResponseGetAllUsersRoles {
+    id?:                 string;
+    email?:              string;
+    name?:               string;
+    lastname?:           string;
+    image?:              string;
+    phone?:              string;
+    password?:           string;
+    notification_token?: string;
+    roles?:              string;
+}
